598ca046ce6a60af299cbfba7500cc22522f6f89
浅爱 <shallowlove21@gmail.com>
2018-03-25 15:29:18 +0800

    Initial Commit

---
+++ Inhual/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Inhual
+//
+//  Created by PatrickY on 2018/3/25.
+//  Copyright © 2018年 runsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Inhual/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Inhual
+//
+//  Created by PatrickY on 2018/3/25.
+//  Copyright © 2018年 runsen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
